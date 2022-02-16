3b026edbb435328b38d42c5094b0e8c8941fbdbe
Nicole Ogawa <nogawa11@gmail.com>
2022-02-16 15:17:08 +0900

    added code and style for restaurants site

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning..."
+Restaurant.destroy_all
+
+puts "Creating 5 restaurants..."
+5.times do
+  Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: Restaurant::CATEGORIES.sample,
+    phone_number: Faker::PhoneNumber.cell_phone
+  )
+end
+
+puts "... created #{Restaurant.count} restaurants."
